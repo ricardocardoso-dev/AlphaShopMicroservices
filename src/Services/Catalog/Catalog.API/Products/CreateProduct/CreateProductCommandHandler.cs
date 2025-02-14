51cf62b9916ec81f38b579bc727da7073524ad30
Ricardo Cardoso <ricardo.cardoso@live.com>
2025-02-13 21:29:26 -0300

    Finished Catalog.API

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommandHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -9,10 +9,10 @@ public class CreateProductCommandValidator : AbstractValidator<CreateProductComm
 {
     public CreateProductCommandValidator()
     {
-        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
-        RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
-        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
-        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required.");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required.");
+        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required.");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0.");
     }
 }
 
