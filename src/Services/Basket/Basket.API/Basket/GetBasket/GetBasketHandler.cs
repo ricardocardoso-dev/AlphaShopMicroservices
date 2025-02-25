b09036141c61e93157174017e9df80545fadcc33
Ricardo Cardoso <ricardo.cardoso@live.com>
2025-02-24 22:03:10 -0300

    (feat) Basket.API - Domain, Endpoints and Handlers

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace Basket.API.Basket.GetBasket;
+
+public record GetBasketResult(ShoppingCart Cart);
+public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+
+public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
+{
+    public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+    {
+        //TODO: get basket from database
+        //var basket = await _repository.GetBasket(request.UserName);
+
+        return new GetBasketResult(new ShoppingCart("swn"));
+    }
+}
