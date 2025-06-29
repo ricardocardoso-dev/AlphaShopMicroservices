0cc7b325c9c4ebf78d5db851f6f1d2de03cf3833
Ricardo Cardoso <ricardo.cardoso@live.com>
2025-06-29 20:07:19 -0300

    feat (Discount): Implementing discount microsservice

---
+++ src/Services/Discount/Discount.gRPC/Data/Extension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.gRPC.Data;
+
+public static class Extension
+{
+    public static IApplicationBuilder UseMigration(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        using var dbContext = scope.ServiceProvider.GetRequiredService<DiscountContext>();
+        dbContext.Database.MigrateAsync();
+
+        return app;
+    }
+}
